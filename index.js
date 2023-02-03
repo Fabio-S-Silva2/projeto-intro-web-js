678766a7c3d6e7eeb9ec1c9ccbba3ef8d087e9a6
Fabio-S-Silva2 <fabio.robert700@gemail.com>
2023-02-03 20:26:05 -0300

    projeto-intro-web

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+
+
 const curso = [
   {
-    nomeCurso:"APIsRest",
-    descricao:"Aprenda tudo dentro de 6 mês",
+    nomeCurso: "APIsRest",
+    descricao: "Aprenda tudo dentro de 6 mês",
     duracao: "Meses 6",
-    valor:2000
+    valor: 2000,
   },
   {
     nomeCurso: "JavaScript",
     descricao: "Aprenda tudo dentro de 2 mês",
     duracao: "Meses 2",
-    valor: 900
+    valor: 900,
   },
   {
     nomeCurso: "HTML e CSS",
     descricao: "Aprenda tudo dentro de 1 mês",
     duracao: "Meses 1",
-    valor: 500
+    valor: 500,
   },
 ];
-const turma = [
+
+////////////////////////////////////////////////////
+const arrayTurma = [
   {
     turma: "Hipátia",
     curso: "JavaScript",
@@ -26,7 +30,7 @@ const turma = [
     terminio: "30/01/2023",
     numeroAlunos: 150,
     periodo: "noturno",
-    concluida: false
+    concluida: false,
   },
   {
     turma: "Sibyla",
@@ -35,7 +39,7 @@ const turma = [
     terminio: "30/12/2022",
     numeroAlunos: 200,
     periodo: "integral",
-    concluida: false
+    concluida: false,
   },
   {
     turma: "Curie",
@@ -44,7 +48,7 @@ const turma = [
     terminio: "15/10/2022",
     numeroAlunos: 180,
     periodo: "noturno",
-    concluida: true
+    concluida: true,
   },
   {
     turma: "Zhenyi",
@@ -53,7 +57,7 @@ const turma = [
     terminio: "01/12/2022",
     numeroAlunos: 80,
     periodo: "integral",
-    concluida: false
+    concluida: false,
   },
   {
     turma: "Clarke",
@@ -62,7 +66,7 @@ const turma = [
     terminio: "04/08/2022",
     numeroAlunos: 200,
     periodo: "noturno",
-    concluida: true
+    concluida: true,
   },
   {
     turma: "Blackwell",
@@ -71,7 +75,7 @@ const turma = [
     terminio: "20/09/2022",
     numeroAlunos: 100,
     periodo: "integral",
-    concluida: true
+    concluida: true,
   },
   {
     turma: "Elion",
@@ -80,7 +84,7 @@ const turma = [
     terminio: "12/07/2022",
     numeroAlunos: 200,
     periodo: "noturno",
-    concluida: true
+    concluida: true,
   },
   {
     turma: "Burnell",
@@ -89,63 +93,64 @@ const turma = [
     terminio: "18/04/2023",
     numeroAlunos: 90,
     periodo: "integral",
-    concluida: false
+    concluida: false,
   },
 ];
-const estudantes = [
+ /////////////////////////////////////////////////////
+const arrayEstudantes = [
   {
-    estudante:"Jean Carlo",
-    turma:"Curie",
-    curso:"HTML e CSS",
-    valor:"500",
-    nParcelas:2,
-    desconto:true,
-    parcelas:200
+    estudante: "Jean Carlo",
+    turma: "Curie",
+    curso: "HTML e CSS",
+    valor: "500",
+    nParcelas: 2,
+    desconto: true,
+    valorParcela: 200,
   },
   {
-    estudante:"Anabelly",
-    turma:"Clarke",
-    curso:"HTML e CSS",
-    valor:"500",
-    nParcelas:1,
-    desconto:true,
-    parcelas:400
+    estudante: "Anabelly",
+    turma: "Clarke",
+    curso: "HTML e CSS",
+    valor: "500",
+    nParcelas: 1,
+    desconto: true,
+    valorParcela: 400,
   },
   {
-    estudante:"Kathleen",
-    turma:"Elion",
-    curso:"APIsRest",
-    valor:"2000",
-    nParcelas:5,
-    desconto:false,
-    parcelas:400
+    estudante: "Kathleen",
+    turma: "Elion",
+    curso: "APIsRest",
+    valor: "2000",
+    nParcelas: 5,
+    desconto: false,
+    valorParcela: 400,
   },
   {
-    estudante:"Adson",
-    turma:"Sibyla",
-    curso:"JavaScript",
-    valor:"900",
-    nParcelas:2,
-    desconto:true,
-    parcelas:360
+    estudante: "Adson",
+    turma: "Sibyla",
+    curso: "JavaScript",
+    valor: "900",
+    nParcelas: 2,
+    desconto: true,
+    valorParcela: 360,
   },
   {
-    estudante:"Lashana Lynch",
-    turma:"Zhenyi",
-    curso:"HTML e CSS",
-    valor:"500",
-    nParcelas:1,
-    desconto:true,
-    parcelas:400
+    estudante: "Lashana Lynch",
+    turma: "Zhenyi",
+    curso: "HTML e CSS",
+    valor: "500",
+    nParcelas: 1,
+    desconto: true,
+    valorParcela: 400,
   },
   {
-    estudante:"Halle Barry",
-    turma:"Burnell",
-    curso:"APIsRest",
-    valor:"2000",
-    nParcelas:4,
-    desconto:false,
-    parcelas:500
+    estudante: "Halle Barry",
+    turma: "Burnell",
+    curso: "APIsRest",
+    valor: "2000",
+    nParcelas: 4,
+    desconto: false,
+    valorParcela: 500,
   },
   {
     estudante: "Chris Evans",
@@ -154,24 +159,136 @@ const estudantes = [
     valor: "900",
     nParcelas: 9,
     desconto: false,
-    parcelas: 100
+    valorParcela: 100,
   },
 ];
-//Função de parcelamento.
-const parcelaCurso=(parcela)=>{
-    let recebe=0
-    let valorComDesconto=0
-    if(parcela>=3&&parcela<=10){
-        recebe+=curso[0].valor/parcela
-        console.log(`O curso ${curso[0].nomeCurso} ficou no valor total de ${curso[0].valor}.\nEm ${parcela}x de ${recebe} reais. `)
 
-    }else if(parcela<=2&&parcela>0){
-        valorComDesconto=curso[0].valor-(curso[0].valor*0.2)
-        recebe=valorComDesconto/parcela,
-        console.log(`O curso ${curso[0].nomeCurso} ficou no valor total de ${valorComDesconto} em ${parcela}x de ${recebe} reais.\nFoi concedido desconto de 20% `)
-    }else{
-        console.log("Suas parcelas não podem ser menores que 1 nem maiores que 10")
+///Função de parcelamento.////////////////////////////
+
+const carrinhoCursos = [];
+
+const adcCarrinho = (nomeDoCurso) => {
+  const curso = buscarCurso(nomeDoCurso);
+  carrinhoCursos.push(curso.valor);
+
+  return carrinhoCursos;
+};
+
+const parcelarCurso = (carrinhoCursos, parcela) => {
+  let valorTotal = 0;
+  let valorPorParcela = 0;
+  let desconto = 0;
+
+  if (carrinhoCursos.length > 1) {
+    switch (carrinhoCursos.length) {
+      case 3:
+        desconto = 0.15;
+        break;
+      case 2:
+        desconto = 0.1;
+        break;
+
+      default:
+        desconto = 0;
+        break;
+    }
+    for (let valor of carrinhoCursos) {
+      valorTotal += valor;
     }
 
-}
-parcelaCurso(prompt("Coloque aqui o número de parcelas que deseja"))
+    valorTotal = valorTotal - valorTotal * desconto;
+  } else {
+    valorTotal = carrinhoCursos[0];
+  }
+
+  if (parcela <= 2) {
+    valorTotal = valorTotal - valorTotal * 0.2;
+    valorPorParcela = valorTotal / parcela;
+    console.log(
+      `O valor do pagamento é de  ${valorTotal} com 20% de desconto, parcelado em ${parcela}x de R$ ${valorPorParcela}. `
+    );
+  } else if (parcela >= 3 && parcela <= 10) {
+    valorTotal = valorTotal;
+    valorPorParcela = valorTotal / parcela;
+    console.log(
+      `O valor do pagamento é de ${valorTotal},parcelado em ${parcela}x de R$ ${valorPorParcela}. `
+    );
+  } else {
+    console.log(
+      `Desculpa! Não trabalhamos acima de 10 parcelas \n Por favor! Escolha um número de parcelas entre 1 e 10.`
+    );
+  }
+};
+console.log(parcelarCurso(carrinhoCursos, 1));
+
+///Função para buscar cursos////////////////////////
+const buscarCurso = (procurarCurso) => {
+  let resultado;
+  for (let cadaCurso of curso) {
+    if (cadaCurso.nomeCurso === procurarCurso) {
+      resultado = cadaCurso;
+      return resultado;
+    } else {
+      return `opção invalida ou curso não existente, ${procurarCurso}`;
+    }
+  }
+};
+console.log(buscarCurso());
+
+/// Função para buscar turma //////////////////
+const buscarTurma = (procurarTurma) => {
+
+  let resultado;
+  for (let cadaTurma of arrayTurma) {
+    if (cadaTurma.turma === procurarTurma) {
+      resultado = cadaTurma;
+      return resultado;
+    }
+  }
+};
+
+/// Função para buscar estudante /////////////////
+const buscarEstudante = (procurarEstudante) => {
+  let resultado;
+  for (let cadaAluno of arrayEstudantes) {
+    if (cadaAluno.estudante === procurarEstudante) {
+      resultado = cadaAluno;
+      return resultado;
+    }
+  }
+};
+
+/// Função matricular /////////////
+const matricular = (nome, curso, turma, numParcelas) => {
+
+  let valorCurso = buscarCurso(curso);
+  let valorTotal = 0;
+  let valorPorParcela = 0;
+  let desconto = false;
+
+  if (numParcelas > 0 && numParcelas <= 2) {
+    desconto = true;
+    valorTotal = valorCurso.valor - valorCurso.valor * 0.2;
+    valorPorParcela = valorTotal / numParcelas;
+  } else {
+    valorTotal = valorCurso.valor;
+    valorPorParcela = valorTotal / numParcelas;
+  }
+
+  let novoAlun = {
+    estudante: nome,
+    turma: turma,
+    curso: curso,
+    valor: valorCurso.valor,
+    nParcelas: numParcelas,
+    desconto: desconto,
+    valorParcela: valorPorParcela,
+  };
+
+  arrayEstudantes.push(novoAlun);
+
+  console.log(arrayEstudantes);
+  console.log(
+    `Aluno matriculado \n Nome:${nome} \n Curso:${curso} \n Turma: ${turma}`
+  );
+};
